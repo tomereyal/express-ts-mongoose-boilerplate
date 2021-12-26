710fdc916714e15d9455ec627c0b7df19b5d66e6
Tomer Eyal <tomereyal93@gmail.com>
2021-12-26 18:01:11 +0200

    added errMissing helper

--- src/_helpers/index.ts
+++ src/_helpers/index.ts
@@ -11,3 +11,9 @@ export function ok(res: Response, body?: Object, debug?: boolean) {
 export function err(res: Response, error: Error) {
   return res.status(500).json({ message: error.message, error });
 }
+
+
+export function errMissing(res: Response) {
+  console.log("ERROR: MISSING REQUEST PARAMS");
+  return res.status(500).json({ message: "Missing request parameter" });
+}
